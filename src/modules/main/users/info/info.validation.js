2cac465c78e540ad2a2440f150516ee548f557aa
ShinCoder <shincoder.forwork@gmail.com>
2023-05-06 23:40:04 +0700

    Change create user validate

--- src/modules/main/users/info/info.validation.js
+++ src/modules/main/users/info/info.validation.js
@@ -5,14 +5,14 @@ export default class InfoValidation {
         try {
             const schema = yup.object({
                 firebase_uid: yup.number().required(),
-                email: yup.string().notRequired(),
-                fullname: yup.string().notRequired(),
-                phone_number: yup.string().notRequired(),
-                gender: yup.mixed().oneOf(['MALE', 'FEMALE']).notRequired(),
-                province_id: yup.number().notRequired(),
-                district_id: yup.number().notRequired(),
-                ward_id: yup.number().notRequired(),
-                address: yup.string().notRequired(),
+                email: yup.string().nullable().notRequired(),
+                fullname: yup.string().nullable().notRequired(),
+                phone_number: yup.string().nullable().notRequired(),
+                gender: yup.mixed().oneOf(['MALE', 'FEMALE', null]).notRequired(),
+                province_id: yup.number().nullable().notRequired(),
+                district_id: yup.number().nullable().notRequired(),
+                ward_id: yup.number().nullable().notRequired(),
+                address: yup.string().nullable().notRequired(),
                 authority: yup.mixed().oneOf(['ADMIN', 'CUSTOMER']).notRequired(),
             })
             return schema.validateSync(req.body);
